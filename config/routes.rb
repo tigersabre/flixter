99cc5b02ee8c7694bf01be655fc661aa7192b9e6
sarunkum <sarosh.arunkumar@appdynamics.com>
2019-03-07 20:40:09 -0800

    allow course creation

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'static_pages#index'
+
+  # instructor namespace
+  namespace :instructor do
+    resources :courses, only: [:new, :create, :show]
+  end
 end
