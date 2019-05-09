27102aaddc9fe36a9c3eb1a2e6686c10f32d8f26
Sarosh Arunkumar <sarosh.arunkumar@gmail.com>
2019-05-09 03:16:29 +0000

    section reorder

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,7 @@ Rails.application.routes.draw do
   # instructor namespace
   namespace :instructor do
     resources :lessons, only: [:update]
+    resources :sections, only: [:update]
     resources :sections, only: [] do
       resources :lessons, only: [:new, :create]
     end
